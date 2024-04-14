4a500828d9ea52af4d3abb8f8ea1de9b84bedb2f
ashley-hansen <ashley.hansen@cqumail.com>
2024-04-15 03:39:50 +1000

    initial commit

---
+++ src/main/java/com/example/hellocqu/HelloCquApplication.java
@@ -0,0 +1,13 @@
+package com.example.hellocqu;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HelloCquApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HelloCquApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/hellocqu/HelloCquApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.hellocqu;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HelloCquApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
